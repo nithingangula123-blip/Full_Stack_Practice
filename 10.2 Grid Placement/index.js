cf4079bb51efa7a5355bd833d1ce40ca5bfa2fa5
Nithin <nithingangula123@gmail.com>
2026-03-16 22:39:12 +0530

    add grid placement javascript files

---
+++ 10.2 Grid Placement/index.js
@@ -0,0 +1,5 @@
+prompt("what is your name?");
+prompt("what is your  her name?");
+var lovescore =Math.random*100;
+lovescore = Math.floor(lovescore)+1;
+alert("your lovescore  is "+ lovescore+"%");
